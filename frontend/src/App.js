6d05e6923452f535f8e5fac467665fd5e8686b8b
unknown <you@example.com>
2021-05-07 07:27:54 -0700

    my first push

---
+++ frontend/src/App.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import ShippingScreen from './screens/ShippingScreen';
+import UserListScreen from './screens/UserListScreen';
+import UserEditScreen from './screens/UserEditScreen';
+import ProductListScreen from './screens/ProductListScreen';
+import ProductEditScreen from './screens/ProductEditScreen';
+
+const App = () => {
+  return (
+    <Router>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Route path='/shipping' component={ShippingScreen} />
+          <Route path='/login' component={LoginScreen} />
+          <Route path='/register' component={RegisterScreen} />
+          <Route path='/product/:id' component={ProductScreen} />
+          <Route path='/cart/:id?' component={CartScreen} />
+          <Route path='/admin/userlist' component={UserListScreen} />
+          <Route path='/admin/user/:id/edit' component={UserEditScreen} />
+          <Route
+            path='/admin/productlist'
+            component={ProductListScreen}
+            exact
+          />
+          <Route
+            path='/admin/productlist/:pageNumber'
+            component={ProductListScreen}
+            exact
+          />
+          <Route path='/admin/product/:id/edit' component={ProductEditScreen} />
+
+          <Route path='/search/:keyword' component={HomeScreen} exact />
+          <Route path='/page/:pageNumber' component={HomeScreen} exact />
+          <Route
+            path='/search/:keyword/page/:pageNumber'
+            component={HomeScreen}
+            exact
+          />
+          <Route path='/' component={HomeScreen} exact />
+        </Container>
+      </main>
+    </Router>
+  );
+};
+
+export default App;
---
+++ frontend/src/screens/ShippingScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ShippingScreen = () => {
+  return (
+    <div>
+      <h3>Thanku for Purchasing our Product...</h3>
+    </div>
+  );
+};
+
+export default ShippingScreen;
